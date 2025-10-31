549c7474724b1f2aade37f3f9202ab0eb6afd97a
Anuj-Devs <anujkatariya1400@gmail.com>
2025-10-31 17:33:45 +0530

    Update next.config.mjs

--- movies-front-end/next.config.mjs
+++ movies-front-end/next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:3001/:path*', // This Path used to be process.env.API_BASE_URL
-      },
-    ];
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
   },
 };
 
